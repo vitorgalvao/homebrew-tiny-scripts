525191451a16c70ff76b94478b8157b118313261
Vítor Galvão <info@vitorgalvao.com>
2014-04-04 04:30:32 +0100

    added casks-repair

---
+++ casks-repair.rb
@@ -0,0 +1,11 @@
+require 'formula'
+
+class CasksRepair < Formula
+  homepage 'https://github.com/vitorgalvao/tiny-scripts'
+  url 'https://github.com/vitorgalvao/tiny-scripts.git'
+  version '0.1'
+
+  def install
+    bin.install 'casks-repair'
+  end
+end
