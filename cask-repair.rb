e9c171501512faa72151e538a3089c68d4d2cb0a
Vítor Galvão <info@vitorgalvao.com>
2014-04-04 04:38:50 +0100

    renamed casks-repair to cask-repair

---
+++ cask-repair.rb
@@ -0,0 +1,11 @@
+require 'formula'
+
+class CaskRepair < Formula
+  homepage 'https://github.com/vitorgalvao/tiny-scripts'
+  url 'https://github.com/vitorgalvao/tiny-scripts.git'
+  version '0.1'
+
+  def install
+    bin.install 'cask-repair'
+  end
+end
