ff02aa3b589f0aec737eda19a13184100090b13e
Vítor Galvão <info@vitorgalvao.com>
2014-05-14 01:10:30 +0100

    updated cask-repair

--- cask-repair.rb
+++ cask-repair.rb
@@ -3,7 +3,7 @@ require 'formula'
 class CaskRepair < Formula
   homepage 'https://github.com/vitorgalvao/tiny-scripts'
   url 'https://github.com/vitorgalvao/tiny-scripts.git'
-  version '0.3.0'
+  version '0.4.0'
 
   def install
     bin.install 'cask-repair'
