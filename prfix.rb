089a6b1e0639bcd3cd7c36a19e263115a4bd3f55
Vítor Galvão <info@vitorgalvao.com>
2014-06-27 15:59:09 +0100

    added prfix

---
+++ prfix.rb
@@ -0,0 +1,11 @@
+require 'formula'
+
+class Prfix < Formula
+  homepage 'https://github.com/vitorgalvao/tiny-scripts'
+  url 'https://github.com/vitorgalvao/tiny-scripts.git'
+  version '0.1'
+
+  def install
+    bin.install 'prfix'
+  end
+end
