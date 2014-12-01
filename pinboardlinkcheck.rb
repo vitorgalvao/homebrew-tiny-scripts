c60df9eb93cb51a5a42f6aa28f222f3edb8a484a
Vítor Galvão <info@vitorgalvao.com>
2014-12-01 03:06:42 +0000

    Update pinboardlinkcheck.rb

--- pinboardlinkcheck.rb
+++ pinboardlinkcheck.rb
@@ -3,7 +3,7 @@ require 'formula'
 class Pinboardlinkcheck < Formula
   homepage 'https://github.com/vitorgalvao/tiny-scripts'
   url 'https://github.com/vitorgalvao/tiny-scripts.git'
-  version '0.2'
+  version '0.2.1'
 
   def install
     bin.install 'pinboardlinkcheck'
