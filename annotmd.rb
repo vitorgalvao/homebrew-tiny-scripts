c60366e74690d42743955137472561ad081c9118
Vítor Galvão <info@vitorgalvao.com>
2015-05-07 23:33:35 +0100

    Update annotmd.rb

--- annotmd.rb
+++ annotmd.rb
@@ -3,7 +3,7 @@ require 'formula'
 class Annotmd < Formula
   homepage 'https://github.com/vitorgalvao/tiny-scripts'
   url 'https://github.com/vitorgalvao/tiny-scripts.git'
-  version '0.1'
+  version '0.1.1'
 
   def install
     bin.install 'annotmd'
