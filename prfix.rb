87d27d0e8f70cc4daa4b035fb2964be7eadefc6c
Vítor Galvão <info@vitorgalvao.com>
2015-06-09 00:26:38 +0100

    prfix.rb: added description

--- prfix.rb
+++ prfix.rb
@@ -1,6 +1,7 @@
 require 'formula'
 
 class Prfix < Formula
+  desc 'Script to quickly pull, fix, and push github pull requests'
   homepage 'https://github.com/vitorgalvao/tiny-scripts'
   url 'https://github.com/vitorgalvao/tiny-scripts.git'
   version '0.3.2'
