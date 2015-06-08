ae6780b94cbe2ec6c045ea0711adc38e5572a56c
Vítor Galvão <info@vitorgalvao.com>
2015-06-09 00:24:05 +0100

    lovecolor.rb: added description

--- lovecolor.rb
+++ lovecolor.rb
@@ -1,6 +1,7 @@
 require 'formula'
 
 class Lovecolor < Formula
+  desc 'Script to get color palette from colourlovers and format it to use in processing'
   homepage 'https://github.com/vitorgalvao/tiny-scripts'
   url 'https://github.com/vitorgalvao/tiny-scripts.git'
   version '0.1'
