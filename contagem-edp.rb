7e9115687f5abd2ccca90e6bec1729049745cc7d
Vítor Galvão <info@vitorgalvao.com>
2015-06-09 00:20:18 +0100

    contagem-edp.rb: added description

--- contagem-edp.rb
+++ contagem-edp.rb
@@ -4,6 +4,7 @@ class ContagemEdp < Formula
   depends_on 'watir-webdriver' => :ruby
   depends_on 'phantomjs'
 
+  desc 'Script to send your energy usage to EDP'
   homepage 'https://github.com/vitorgalvao/tiny-watir-scripts'
   url 'https://github.com/vitorgalvao/tiny-watir-scripts.git'
   version '0.1'
