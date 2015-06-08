3ae255b0b7ef90d2299d0708033320a55deadb9c
Vítor Galvão <info@vitorgalvao.com>
2015-06-09 00:28:26 +0100

    strongpassword.rb: added description

--- strongpassword.rb
+++ strongpassword.rb
@@ -1,6 +1,7 @@
 require 'formula'
 
 class Strongpassword < Formula
+  desc 'Script to get a strong password by leveraging multiple sources'
   homepage 'https://github.com/vitorgalvao/tiny-scripts'
   url 'https://github.com/vitorgalvao/tiny-scripts.git'
   version '0.2'
