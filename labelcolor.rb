5d62300be174840a26a1941db21b7bdb3e178545
Vítor Galvão <info@vitorgalvao.com>
2015-06-09 00:23:44 +0100

    labelcolor.rb: added description

--- labelcolor.rb
+++ labelcolor.rb
@@ -1,6 +1,7 @@
 require 'formula'
 
 class Labelcolor < Formula
+  desc 'Script to change label color of files in OS X'
   homepage 'https://github.com/vitorgalvao/tiny-scripts'
   url 'https://github.com/vitorgalvao/tiny-scripts.git'
   version '0.1'
