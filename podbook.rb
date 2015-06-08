58b9b336185b954689a33b21821485f8d827a6f5
Vítor Galvão <info@vitorgalvao.com>
2015-06-09 00:26:13 +0100

    podbook.rb: added description

--- podbook.rb
+++ podbook.rb
@@ -1,6 +1,7 @@
 require 'formula'
 
 class Podbook < Formula
+  desc 'Script to generate a podcast RSS feed for audiobooks'
   homepage 'https://github.com/vitorgalvao/tiny-scripts'
   url 'https://github.com/vitorgalvao/tiny-scripts.git'
   version '0.2.1'
