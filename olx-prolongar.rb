e626be19ff8e98e98830bf6448618d4a91ba35c6
Vítor Galvão <info@vitorgalvao.com>
2015-06-09 00:25:01 +0100

    olx-prolongar.rb: added description

--- olx-prolongar.rb
+++ olx-prolongar.rb
@@ -4,6 +4,7 @@ class OlxProlongar < Formula
   depends_on 'watir-webdriver' => :ruby
   depends_on 'phantomjs'
 
+  desc 'Script to automate the renewal of OLX expiring ads'
   homepage 'https://github.com/vitorgalvao/tiny-watir-scripts'
   url 'https://github.com/vitorgalvao/tiny-watir-scripts.git'
   version '0.2.2'
