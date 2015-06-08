93bfdbd7b90c29f40160d06f2509efd3456d74c8
Vítor Galvão <info@vitorgalvao.com>
2015-06-09 00:24:35 +0100

    olx-post.rb: added description

--- olx-post.rb
+++ olx-post.rb
@@ -4,6 +4,7 @@ class OlxPost < Formula
   depends_on 'watir-webdriver' => :ruby
   depends_on 'phantomjs'
 
+  desc 'Script to post ads to OLX quicker'
   homepage 'https://github.com/vitorgalvao/tiny-watir-scripts'
   url 'https://github.com/vitorgalvao/tiny-watir-scripts.git'
   version '0.1.2'
