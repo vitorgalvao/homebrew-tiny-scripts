7c0e2489e9389805a7a69853723939b68abc6798
Vítor Galvão <info@vitorgalvao.com>
2015-06-09 00:21:15 +0100

    customise-terminal-notifier.rb: added description

--- customise-terminal-notifier.rb
+++ customise-terminal-notifier.rb
@@ -1,6 +1,7 @@
 require 'formula'
 
 class CustomiseTerminalNotifier < Formula
+  desc 'Script to setup terminal-notifier with a custom icon'
   homepage 'https://github.com/vitorgalvao/tiny-scripts'
   url 'https://github.com/vitorgalvao/tiny-scripts.git'
   version '0.2'
