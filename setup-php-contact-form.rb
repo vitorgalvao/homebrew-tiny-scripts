f7946b032cdd064f1e77ebe7f4ace776753a8a3f
Vítor Galvão <info@vitorgalvao.com>
2015-06-09 00:31:50 +0100

    setup-php-contact-form.rb: added description

--- setup-php-contact-form.rb
+++ setup-php-contact-form.rb
@@ -1,6 +1,7 @@
 require 'formula'
 
 class SetupPhpContactForm < Formula
+  desc 'Setup for easy start of a PHP contact form'
   homepage 'https://github.com/vitorgalvao/setup-php-contact-form'
   url 'https://github.com/vitorgalvao/setup-php-contact-form.git'
   version '0.1'
