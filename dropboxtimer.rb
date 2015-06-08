a1e7d99a5782f84f799138421e899579b1f3aec2
Vítor Galvão <info@vitorgalvao.com>
2015-06-09 00:21:34 +0100

    dropboxtimer.rb: added description

--- dropboxtimer.rb
+++ dropboxtimer.rb
@@ -1,6 +1,7 @@
 require 'formula'
 
 class Dropboxtimer < Formula
+  desc 'Script to add files to your dropbox, and delete them a set number of days later'
   homepage 'https://github.com/vitorgalvao/tiny-scripts'
   url 'https://github.com/vitorgalvao/tiny-scripts.git'
   version '0.1'
