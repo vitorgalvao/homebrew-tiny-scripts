2ef0ab5818a29cfe93a8d9d62bb48dd114e78e34
Vítor Galvão <info@vitorgalvao.com>
2015-06-09 00:28:45 +0100

    ubuntu-usb.rb: added description

--- ubuntu-usb.rb
+++ ubuntu-usb.rb
@@ -1,6 +1,7 @@
 require 'formula'
 
 class UbuntuUsb < Formula
+  desc 'Script to facilitate installing ubuntu on a USB stick, from OS X'
   homepage 'https://github.com/vitorgalvao/tiny-scripts'
   url 'https://github.com/vitorgalvao/tiny-scripts.git'
   version '0.1'
