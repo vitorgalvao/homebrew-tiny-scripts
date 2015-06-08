5c2c2561809c7ad5c2977ee4cb39ef2cfe464289
Vítor Galvão <info@vitorgalvao.com>
2015-06-09 00:18:58 +0100

    annotmd.rb: added description

--- annotmd.rb
+++ annotmd.rb
@@ -1,6 +1,7 @@
 require 'formula'
 
 class Annotmd < Formula
+  desc 'Script to convert .annot files to .md'
   homepage 'https://github.com/vitorgalvao/tiny-scripts'
   url 'https://github.com/vitorgalvao/tiny-scripts.git'
   version '0.1.1'
