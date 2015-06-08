d97ef7f21ed627415c4b5f7b61f5413a0df5e5cc
Vítor Galvão <info@vitorgalvao.com>
2015-06-09 00:27:56 +0100

    seren.rb: added description

--- seren.rb
+++ seren.rb
@@ -1,6 +1,7 @@
 require 'formula'
 
 class Seren < Formula
+  desc 'Script to rename files in a numerical sequence'
   homepage 'https://github.com/vitorgalvao/tiny-scripts'
   url 'https://github.com/vitorgalvao/tiny-scripts.git'
   version '0.2.1'
