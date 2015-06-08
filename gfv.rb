e854416c64f40dc93c499419e2c82667a98a6b2e
Vítor Galvão <info@vitorgalvao.com>
2015-06-09 00:22:01 +0100

    gfv.rb: added description

--- gfv.rb
+++ gfv.rb
@@ -4,6 +4,7 @@ class Gfv < Formula
   depends_on 'imagemagick'
   depends_on 'ffmpeg'
 
+  desc 'Script to make animated gifs from sequences of images, with more control over FPS'
   homepage 'https://github.com/vitorgalvao/tiny-scripts'
   url 'https://github.com/vitorgalvao/tiny-scripts.git'
   version '0.2.1'
