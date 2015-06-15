2c2743603b6e9859789579d02af0ab728fc41755
Vítor Galvão <info@vitorgalvao.com>
2015-06-15 20:44:18 +0100

    Update and rename prfix.rb to prfixbranch.rb

---
+++ prfixbranch.rb
@@ -0,0 +1,12 @@
+require 'formula'
+
+class Prfixbranch < Formula
+  desc 'Script to quickly pull, fix, and push github pull requests, creating a new pull request with the fix'
+  homepage 'https://github.com/vitorgalvao/tiny-scripts'
+  url 'https://github.com/vitorgalvao/tiny-scripts.git'
+  version '0.3.3'
+
+  def install
+    bin.install 'prfixbranch'
+  end
+end
