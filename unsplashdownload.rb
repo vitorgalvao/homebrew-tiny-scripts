4fa3fc27b804271d9f6342c4dddc0bab803e2964
Vítor Galvão <info@vitorgalvao.com>
2015-06-22 20:28:25 +0100

    Update unsplashdownload.rb

--- unsplashdownload.rb
+++ unsplashdownload.rb
@@ -1,7 +1,7 @@
 require 'formula'
 
 class Unsplashdownload < Formula
-  desc 'Script to download images from unsplash'
+  desc 'Script to download images from unsplash’s last batch'
   homepage 'https://github.com/vitorgalvao/tiny-scripts'
   url 'https://github.com/vitorgalvao/tiny-scripts.git'
   version '0.1'
