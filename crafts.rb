b7c6031cd0ea37282ac3095401336791db96cd80
Vítor Galvão <info@vitorgalvao.com>
2015-06-23 15:09:37 +0100

    Update crafts.rb

--- crafts.rb
+++ crafts.rb
@@ -4,7 +4,7 @@ class Crafts < Formula
   desc 'Script to allow copy/cut/paste between different terminal windows'
   homepage 'https://github.com/vitorgalvao/tiny-scripts'
   url 'https://github.com/vitorgalvao/tiny-scripts.git'
-  version '0.1'
+  version '0.1.1'
 
   def install
     (prefix/'etc/profile.d').install 'crafts'
