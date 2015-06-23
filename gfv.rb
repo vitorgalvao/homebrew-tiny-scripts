5b57c69526a79fe1bf69c0cc0c3831b90078f455
Vítor Galvão <info@vitorgalvao.com>
2015-06-23 15:09:10 +0100

    Update gfv.rb

--- gfv.rb
+++ gfv.rb
@@ -7,7 +7,7 @@ class Gfv < Formula
   desc 'Script to make animated gifs from a video file'
   homepage 'https://github.com/vitorgalvao/tiny-scripts'
   url 'https://github.com/vitorgalvao/tiny-scripts.git'
-  version '0.2.1'
+  version '0.2.2'
 
   def install
     bin.install 'gfv'
