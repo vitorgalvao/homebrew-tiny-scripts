da5ec4c6989d67cbae244e8cf5a6480e8f673fa4
Vítor Galvão <info@vitorgalvao.com>
2015-09-02 19:41:11 +0100

    Update contagem-edp.rb

--- contagem-edp.rb
+++ contagem-edp.rb
@@ -7,7 +7,7 @@ class ContagemEdp < Formula
   desc 'Script to send your energy usage to EDP'
   homepage 'https://github.com/vitorgalvao/tiny-watir-scripts'
   url 'https://github.com/vitorgalvao/tiny-watir-scripts.git'
-  version '0.1'
+  version '0.1.1'
 
   def install
     bin.install 'contagem-edp'
