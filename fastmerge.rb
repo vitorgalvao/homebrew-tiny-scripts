328fe3aed0956d9f04d7b0bb97a96be01c1811b0
Vítor Galvão <info@vitorgalvao.com>
2015-12-20 16:31:15 +0000

    fastmerge.rb: add dependency on ghi

--- fastmerge.rb
+++ fastmerge.rb
@@ -1,4 +1,6 @@
 class Fastmerge < Formula
+  depends_on 'ghi'
+
   desc 'Script to quickly pull and push github pull requests to keep commit history cleaner, merging directly to master and closing the previous pull request'
   homepage 'https://github.com/vitorgalvao/tiny-scripts'
   url 'https://github.com/vitorgalvao/tiny-scripts.git'
