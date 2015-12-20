5867d90eb802d9a58e949a16d4e183df116be947
Vítor Galvão <info@vitorgalvao.com>
2015-12-20 16:30:48 +0000

    prfixmaster.rb: add dependency on ghi

--- prfixmaster.rb
+++ prfixmaster.rb
@@ -1,4 +1,6 @@
 class Prfixmaster < Formula
+  depends_on 'ghi'
+
   desc 'Script to quickly pull, fix, and push github pull requests, merging directly to master and closing the previous pull request'
   homepage 'https://github.com/vitorgalvao/tiny-scripts'
   url 'https://github.com/vitorgalvao/tiny-scripts.git'
