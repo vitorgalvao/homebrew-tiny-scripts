b724f0774510ff5962b3674a9fb2aa32cce05d97
Vítor Galvão <info@vitorgalvao.com>
2016-01-06 20:21:29 +0000

    cask-repair.rb: add hr dependency

--- cask-repair.rb
+++ cask-repair.rb
@@ -1,10 +1,11 @@
 class CaskRepair < Formula
   depends_on 'hub'
+  depends_on 'hr'
 
   desc 'Script to quickly repair outdated/broken Casks from homebrew-cask'
   homepage 'https://github.com/vitorgalvao/tiny-scripts'
   url 'https://github.com/vitorgalvao/tiny-scripts.git'
-  version '0.12.7'
+  version '0.12.8'
 
   def install
     bin.install 'cask-repair'
