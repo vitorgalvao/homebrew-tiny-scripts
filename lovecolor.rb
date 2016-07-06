788274d47c519bcab8c77f5078cff13a50f0ea15
Vítor Galvão <info@vitorgalvao.com>
2016-07-06 16:04:08 +0100

    Update lovecolor.rb

--- lovecolor.rb
+++ lovecolor.rb
@@ -2,7 +2,7 @@ class Lovecolor < Formula
   desc 'Script to get color palette from colourlovers and format it to use in processing'
   homepage 'https://github.com/vitorgalvao/tiny-scripts'
   url 'https://github.com/vitorgalvao/tiny-scripts.git'
-  version '0.1'
+  version '0.1.0'
 
   def install
     bin.install 'lovecolor'
