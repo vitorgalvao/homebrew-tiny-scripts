a4788f969eec033a9ec95f1ee37ca2b05cc562ea
Vítor Galvão <info@vitorgalvao.com>
2016-07-06 16:04:32 +0100

    Update strongpassword.rb

--- strongpassword.rb
+++ strongpassword.rb
@@ -2,7 +2,7 @@ class Strongpassword < Formula
   desc 'Script to get a strong password by leveraging multiple sources'
   homepage 'https://github.com/vitorgalvao/tiny-scripts'
   url 'https://github.com/vitorgalvao/tiny-scripts.git'
-  version '0.2'
+  version '0.2.0'
 
   def install
     bin.install 'strongpassword'
