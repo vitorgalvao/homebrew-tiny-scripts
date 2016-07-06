ad16a8a55683765628cd302d9507e809293866ee
Vítor Galvão <info@vitorgalvao.com>
2016-07-06 16:04:46 +0100

    Update ubuntu-usb.rb

--- ubuntu-usb.rb
+++ ubuntu-usb.rb
@@ -2,7 +2,7 @@ class UbuntuUsb < Formula
   desc 'Script to facilitate installing ubuntu on a USB stick, from OS X'
   homepage 'https://github.com/vitorgalvao/tiny-scripts'
   url 'https://github.com/vitorgalvao/tiny-scripts.git'
-  version '0.1'
+  version '0.1.0'
 
   def install
     bin.install 'ubuntu-usb'
