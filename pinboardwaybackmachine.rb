caca066e40ca3cac696d4aff187967b420f6a232
Vítor Galvão <info@vitorgalvao.com>
2016-08-03 00:35:56 +0100

    Create pinboardwaybackmachine.rb

---
+++ pinboardwaybackmachine.rb
@@ -0,0 +1,10 @@
+class Pinboardwaybackmachine < Formula
+  desc 'Script to add to the wayback machine links saved in your pinboard account'
+  homepage 'https://github.com/vitorgalvao/tiny-scripts'
+  url 'https://github.com/vitorgalvao/tiny-scripts.git'
+  version '0.0.1'
+
+  def install
+    bin.install 'pinboardwaybackmachine'
+  end
+end
