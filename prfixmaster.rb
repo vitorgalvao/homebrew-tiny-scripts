73e2b700e5814b06f4a85645e706ec3677cbbb6a
Vítor Galvão <info@vitorgalvao.com>
2016-08-08 12:39:07 +0100

    Update prfixmaster.rb

--- prfixmaster.rb
+++ prfixmaster.rb
@@ -4,7 +4,7 @@ class Prfixmaster < Formula
   desc 'Script to quickly pull, fix, and push github pull requests, merging directly to master and closing the previous pull request'
   homepage 'https://github.com/vitorgalvao/tiny-scripts'
   url 'https://github.com/vitorgalvao/tiny-scripts.git'
-  version '0.6.1'
+  version '0.7.0'
 
   def install
     bin.install 'prfixmaster'
