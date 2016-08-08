4944780476f03593d76a45f13e680158c8929e36
Vítor Galvão <info@vitorgalvao.com>
2016-08-08 12:38:42 +0100

    Update prfixbranch.rb

--- prfixbranch.rb
+++ prfixbranch.rb
@@ -2,7 +2,7 @@ class Prfixbranch < Formula
   desc 'Script to quickly pull, fix, and push github pull requests, creating a new pull request with the fix'
   homepage 'https://github.com/vitorgalvao/tiny-scripts'
   url 'https://github.com/vitorgalvao/tiny-scripts.git'
-  version '0.5.4'
+  version '0.6.0'
 
   def install
     bin.install 'prfixbranch'
