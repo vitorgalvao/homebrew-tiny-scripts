62a6894cd4f0c143e883cdb30f0e892e18b3a217
Vítor Galvão <info@vitorgalvao.com>
2016-09-05 12:43:07 +0100

    Create suntimes.rb

---
+++ suntimes.rb
@@ -0,0 +1,10 @@
+class Suntimes < Formula
+  desc 'Script to email local sunrise and sunset times (via Wolfram Alpha) for a given location'
+  homepage 'https://github.com/vitorgalvao/tiny-scripts'
+  url 'https://github.com/vitorgalvao/tiny-scripts.git'
+  version '0.0.1'
+
+  def install
+    bin.install 'suntimes'
+  end
+end
