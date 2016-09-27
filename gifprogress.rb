5ed7d653027c9476ad220d3b8cfdd9fb3aa2e61e
Vítor Galvão <info@vitorgalvao.com>
2016-09-28 00:07:43 +0100

    Create gifprogress.rb

---
+++ gifprogress.rb
@@ -0,0 +1,13 @@
+class Gifprogress < Formula
+  depends_on 'imagemagick'
+  depends_on 'ffmpeg'
+
+  desc 'Script to convert a sequence of images to a gif, with a progress bar'
+  homepage 'https://github.com/vitorgalvao/tiny-scripts'
+  url 'https://github.com/vitorgalvao/tiny-scripts.git'
+  version '0.0.1'
+
+  def install
+    bin.install 'gifprogress'
+  end
+end
