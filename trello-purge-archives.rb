6a7d328548e12edeeba04e2c7513137994c60314
Vítor Galvão <info@vitorgalvao.com>
2016-10-12 14:48:41 +0100

    Update trello-purge-archives.rb

--- trello-purge-archives.rb
+++ trello-purge-archives.rb
@@ -4,7 +4,7 @@ class TrelloPurgeArchives < Formula
   desc 'Script to permanently delete all archived cards and cards in closed boards on Trello'
   homepage 'https://github.com/vitorgalvao/tiny-watir-scripts'
   url 'https://github.com/vitorgalvao/tiny-watir-scripts.git'
-  version '0.0.1'
+  version '0.0.2'
 
   def install
     bin.install 'trello-purge-archives'
