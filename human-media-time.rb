33e1c9be298070b077c4a2f32a86cd438b047a07
Vítor Galvão <info@vitorgalvao.com>
2017-01-15 14:24:33 +0000

    Update human-media-time.rb

--- human-media-time.rb
+++ human-media-time.rb
@@ -4,7 +4,7 @@ class HumanMediaTime < Formula
   desc 'Script to calculate the total running time of multiple media files, presenting the output in a human-readable way'
   homepage 'https://github.com/vitorgalvao/tiny-scripts'
   url 'https://github.com/vitorgalvao/tiny-scripts.git'
-  version '0.1.0'
+  version '0.1.1'
 
   def install
     bin.install 'human-media-time'
