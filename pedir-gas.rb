6e3f5f26ffa4991cf0e4686e5b88468805605283
Vítor Galvão <info@vitorgalvao.com>
2017-03-09 13:31:16 +0000

    Update pedir-gas.rb

--- pedir-gas.rb
+++ pedir-gas.rb
@@ -5,7 +5,7 @@ class PedirGas < Formula
   desc 'Script to ask gas delivery from drogaria Ponto Azul'
   homepage 'https://github.com/vitorgalvao/tiny-watir-scripts'
   url 'https://github.com/vitorgalvao/tiny-watir-scripts.git'
-  version '0.3.0'
+  version '0.4.0'
 
   def install
     bin.install 'pedir-gas'
