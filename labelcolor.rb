a36fd12af8d7d0d72a9f61bff34e2a6acaaf55a1
Vítor Galvão <info@vitorgalvao.com>
2017-04-15 14:14:57 +0100

    Update labelcolor.rb

--- labelcolor.rb
+++ labelcolor.rb
@@ -2,7 +2,7 @@ class Labelcolor < Formula
   desc 'Script to change label color of files in OS X'
   homepage 'https://github.com/vitorgalvao/tiny-scripts'
   url 'https://github.com/vitorgalvao/tiny-scripts.git'
-  version '0.1.3'
+  version '0.1.4'
 
   def install
     bin.install 'labelcolor'
