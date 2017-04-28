e4e3d2be0c272930ce7fc023e173bf88e1908659
Vítor Galvão <info@vitorgalvao.com>
2017-04-28 20:12:12 +0100

    Update fastmerge.rb

--- fastmerge.rb
+++ fastmerge.rb
@@ -1,7 +1,7 @@
 class Fastmerge < Formula
   depends_on 'ghi'
 
-  desc 'Script to quickly pull and push github pull requests to keep commit history cleaner, merging directly to master and closing the previous pull request'
+  desc 'Script to quickly pull and push github pull requests to keep commit history cleaner, merging directly to master and closing the pull request'
   homepage 'https://github.com/vitorgalvao/tiny-scripts'
   url 'https://github.com/vitorgalvao/tiny-scripts.git'
   version '0.14.0'
