d85399ce02c731a317acb6d3948e95b9d9736529
Vítor Galvão <info@vitorgalvao.com>
2017-04-29 14:07:20 +0100

    Create pinboardbackup.rb

---
+++ pinboardbackup.rb
@@ -0,0 +1,10 @@
+class Pinboardbackup < Formula
+  desc 'Script to backup all bookmarks in your pinboard account'
+  homepage 'https://github.com/vitorgalvao/tiny-scripts'
+  url 'https://github.com/vitorgalvao/tiny-scripts.git'
+  version '0.0.1'
+
+  def install
+    bin.install 'pinboardbackup'
+  end
+end
