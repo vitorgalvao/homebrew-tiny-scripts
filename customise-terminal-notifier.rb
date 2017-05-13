6e83064ca330634a9ca8c2503cc1c3e027069230
Vítor Galvão <info@vitorgalvao.com>
2017-05-14 00:55:36 +0100

    Update customise-terminal-notifier.rb

--- customise-terminal-notifier.rb
+++ customise-terminal-notifier.rb
@@ -2,7 +2,7 @@ class CustomiseTerminalNotifier < Formula
   desc 'Script to setup terminal-notifier with a custom icon'
   homepage 'https://github.com/vitorgalvao/tiny-scripts'
   url 'https://github.com/vitorgalvao/tiny-scripts.git'
-  version '0.4.4'
+  version '0.5.0'
 
   def install
     bin.install 'customise-terminal-notifier'
