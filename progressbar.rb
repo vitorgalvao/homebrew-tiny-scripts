49d83997935a9d1d9f03a4d1a0616f38122ae82f
Vítor Galvão <info@vitorgalvao.com>
2017-05-20 00:32:13 +0100

    Update progressbar.rb

--- progressbar.rb
+++ progressbar.rb
@@ -2,7 +2,7 @@ class Progressbar < Formula
   depends_on 'imagemagick'
   depends_on 'ffmpeg'
 
-  desc 'Overlay a progress bar on moving pictures'
+  desc 'Overlay a progress bar on videos or gifs'
   homepage 'https://github.com/vitorgalvao/tiny-scripts'
   url 'https://github.com/vitorgalvao/tiny-scripts.git'
   version '0.0.1'
