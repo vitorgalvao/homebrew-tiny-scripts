d4a7436c51f2e0c0e7504a4037aa89b892350180
Vítor Galvão <info@vitorgalvao.com>
2017-05-19 19:30:37 +0100

    Update gifprogress.rb

--- gifprogress.rb
+++ gifprogress.rb
@@ -2,7 +2,7 @@ class Gifprogress < Formula
   depends_on 'imagemagick'
   depends_on 'ffmpeg'
 
-  desc 'Script to convert a sequence of images to a gif, with a progress bar'
+  desc 'Script to convert a sequence of images or video to a gif or video with a progress bar'
   homepage 'https://github.com/vitorgalvao/tiny-scripts'
   url 'https://github.com/vitorgalvao/tiny-scripts.git'
   version '0.1.0'
