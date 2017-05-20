46dce319e4f17d93e03c57194bb5abad4811a42b
Vítor Galvão <info@vitorgalvao.com>
2017-05-20 15:25:30 +0100

    Update pinboardbackup.rb

--- pinboardbackup.rb
+++ pinboardbackup.rb
@@ -2,7 +2,7 @@ class Pinboardbackup < Formula
   desc 'Script to backup all bookmarks in your pinboard account'
   homepage 'https://github.com/vitorgalvao/tiny-scripts'
   url 'https://github.com/vitorgalvao/tiny-scripts.git'
-  version '0.0.1'
+  version '0.0.2'
 
   def install
     bin.install 'pinboardbackup'
