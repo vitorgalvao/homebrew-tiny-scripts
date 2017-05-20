a377ebe855cd332bf177b440896a329833e28873
Vítor Galvão <info@vitorgalvao.com>
2017-05-20 15:39:47 +0100

    Update setup-php-contact-form.rb

--- setup-php-contact-form.rb
+++ setup-php-contact-form.rb
@@ -2,7 +2,7 @@ class SetupPhpContactForm < Formula
   desc 'Setup for easy start of a PHP contact form'
   homepage 'https://github.com/vitorgalvao/setup-php-contact-form'
   url 'https://github.com/vitorgalvao/setup-php-contact-form.git'
-  version '0.1.0'
+  version '0.1.1'
 
   def install
     bin.install 'setup-php-contact-form'
