e62040ff8790ab6e31f7f2beea47eaedc2df9b2f
Vítor Galvão <info@vitorgalvao.com>
2017-06-18 01:13:43 +0100

    Update olx-post.rb

--- olx-post.rb
+++ olx-post.rb
@@ -5,7 +5,7 @@ class OlxPost < Formula
   desc 'Script to post ads to OLX quicker'
   homepage 'https://github.com/vitorgalvao/tiny-watir-scripts'
   url 'https://github.com/vitorgalvao/tiny-watir-scripts.git'
-  version '0.1.2'
+  version '0.1.3'
 
   def install
     bin.install 'olx-post'
