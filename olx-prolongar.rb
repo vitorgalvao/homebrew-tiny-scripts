51d8f355b789874b127e1bbbd65e4337bd2c0a5c
Vítor Galvão <info@vitorgalvao.com>
2017-10-26 01:25:46 +0100

    Update olx-prolongar.rb

--- olx-prolongar.rb
+++ olx-prolongar.rb
@@ -5,7 +5,7 @@ class OlxProlongar < Formula
   desc 'Script to automate the renewal of OLX expiring ads'
   homepage 'https://github.com/vitorgalvao/tiny-watir-scripts'
   url 'https://github.com/vitorgalvao/tiny-watir-scripts.git'
-  version '0.3.2'
+  version '0.3.3'
 
   def install
     bin.install 'olx-prolongar'
