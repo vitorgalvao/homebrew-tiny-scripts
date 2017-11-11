be6dc6cb5e717213e55c887424640cdddb2f2954
Vítor Galvão <info@vitorgalvao.com>
2017-11-11 16:25:11 +0000

    Create macspoof.rb

---
+++ macspoof.rb
@@ -0,0 +1,10 @@
+class Macspoof < Formula
+  desc 'Script to spoof the MAC address'
+  homepage 'https://github.com/vitorgalvao/tiny-scripts'
+  url 'https://github.com/vitorgalvao/tiny-scripts.git'
+  version '0.0.1'
+
+  def install
+    bin.install 'macspoof'
+  end
+end
