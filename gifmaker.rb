e24a5b04f29b01d1eacb066ef1272834429aa889
Vítor Galvão <info@vitorgalvao.com>
2018-01-26 16:21:40 +0000

    Update gifmaker.rb

--- gifmaker.rb
+++ gifmaker.rb
@@ -1,7 +1,7 @@
 class Gifmaker < Formula
   depends_on 'imagemagick'
 
-  desc 'Script to make animated gifs from sequences of images, with more control over FPS'
+  desc 'Script to make animated gifs from sequences of images, with control over FPS'
   homepage 'https://github.com/vitorgalvao/tiny-scripts'
   url 'https://github.com/vitorgalvao/tiny-scripts.git'
   version '0.3.4'
