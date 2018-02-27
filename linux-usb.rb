7ba18b54bf08edafaee0a5181284a660d5fb0f2d
Vítor Galvão <info@vitorgalvao.com>
2018-02-27 15:04:18 +0000

    Update linux-usb.rb

--- linux-usb.rb
+++ linux-usb.rb
@@ -2,7 +2,7 @@ class LinuxUsb < Formula
   desc 'Script to facilitate installing Linux on a USB stick, from macOS'
   homepage 'https://github.com/vitorgalvao/tiny-scripts'
   url 'https://github.com/vitorgalvao/tiny-scripts.git'
-  version '0.0.1'
+  version '0.0.2'
 
   def install
     bin.install 'linux-usb'
