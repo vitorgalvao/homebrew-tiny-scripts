a5c76adfbd7ce545123d863c608aeba0a85cb5a1
Vítor Galvão <info@vitorgalvao.com>
2018-04-14 20:19:24 +0100

    pedir-gas.rb: removed chromedriver dependency

--- pedir-gas.rb
+++ pedir-gas.rb
@@ -1,6 +1,4 @@
 class PedirGas < Formula
-  depends_on 'chromedriver'
-
   desc 'Script to ask gas delivery from drogaria Ponto Azul'
   homepage 'https://github.com/vitorgalvao/tiny-watir-scripts'
   url 'https://github.com/vitorgalvao/tiny-watir-scripts.git'
