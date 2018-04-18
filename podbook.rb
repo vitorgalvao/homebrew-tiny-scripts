3041b5291a1b20a41bf111237f559cfd79df85ae
Vítor Galvão <info@vitorgalvao.com>
2018-04-18 13:27:19 +0100

    Update podbook.rb

--- podbook.rb
+++ podbook.rb
@@ -4,7 +4,7 @@ class Podbook < Formula
   desc 'Script to generate a podcast RSS feed for audiobooks'
   homepage 'https://github.com/vitorgalvao/tiny-scripts'
   url 'https://github.com/vitorgalvao/tiny-scripts.git'
-  version '0.13.0'
+  version '0.13.1'
 
   def install
     bin.install 'podbook'
