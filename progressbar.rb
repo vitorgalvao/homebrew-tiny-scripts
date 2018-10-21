097acec24f225539a88b2bd0fef096d4ca93fb91
Vítor Galvão <info@vitorgalvao.com>
2018-10-21 18:03:52 +0100

    Update progressbar.rb

--- progressbar.rb
+++ progressbar.rb
@@ -6,7 +6,7 @@ class Progressbar < Formula
   desc 'Overlay a progress bar on videos or gifs'
   homepage 'https://github.com/vitorgalvao/tiny-scripts'
   url 'https://github.com/vitorgalvao/tiny-scripts.git'
-  version '0.0.2'
+  version '0.0.3'
 
   def install
     bin.install 'progressbar'
