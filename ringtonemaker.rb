66fe164aabebe50e9fb339ffa399c0aaf8181541
Vítor Galvão <info@vitorgalvao.com>
2018-10-21 18:04:08 +0100

    Update ringtonemaker.rb

--- ringtonemaker.rb
+++ ringtonemaker.rb
@@ -4,7 +4,7 @@ class Ringtonemaker < Formula
   desc 'Script to convert a video or audio file to an iPhone ringtone'
   homepage 'https://github.com/vitorgalvao/tiny-scripts'
   url 'https://github.com/vitorgalvao/tiny-scripts.git'
-  version '0.0.4'
+  version '0.0.5'
 
   def install
     bin.install 'ringtonemaker'
