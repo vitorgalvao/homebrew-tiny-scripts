aac9675f824feccd7bdc6fb43dd85c5b2f8c7450
Vítor Galvão <info@vitorgalvao.com>
2018-10-21 18:04:39 +0100

    Update fastmerge.rb

--- fastmerge.rb
+++ fastmerge.rb
@@ -4,7 +4,7 @@ class Fastmerge < Formula
   desc 'Script to quickly pull and push github pull requests to keep commit history cleaner, merging directly to master and closing the pull request'
   homepage 'https://github.com/vitorgalvao/tiny-scripts'
   url 'https://github.com/vitorgalvao/tiny-scripts.git'
-  version '0.14.0'
+  version '0.14.1'
 
   def install
     bin.install 'fastmerge'
