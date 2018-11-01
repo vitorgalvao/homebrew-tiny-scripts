44db5f3fed9f7f877979bf943e374f73dbcdd1fa
Vítor Galvão <info@vitorgalvao.com>
2018-11-01 17:38:51 +0000

    Update pinboardurlupdate.rb

--- pinboardurlupdate.rb
+++ pinboardurlupdate.rb
@@ -2,7 +2,7 @@ class Pinboardurlupdate < Formula
   desc 'Script to substitute URLs of pinboard bookmarks'
   homepage 'https://github.com/vitorgalvao/tiny-scripts'
   url 'https://github.com/vitorgalvao/tiny-scripts.git'
-  version '0.0.1'
+  version '0.0.2'
 
   def install
     bin.install 'pinboardurlupdate'
