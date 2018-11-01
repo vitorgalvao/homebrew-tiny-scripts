885252bf50e90d24692b6a0dc988c994415a3ba0
Vítor Galvão <info@vitorgalvao.com>
2018-11-01 17:38:45 +0000

    Update pinboardlinkcheck.rb

--- pinboardlinkcheck.rb
+++ pinboardlinkcheck.rb
@@ -2,7 +2,7 @@ class Pinboardlinkcheck < Formula
   desc 'Script to check the status code of links saved in your pinboard account'
   homepage 'https://github.com/vitorgalvao/tiny-scripts'
   url 'https://github.com/vitorgalvao/tiny-scripts.git'
-  version '0.7.0'
+  version '0.7.1'
 
   def install
     bin.install 'pinboardlinkcheck'
