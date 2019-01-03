02b22525ed24f6c3621a8c0a2e6d91993a1f64a3
Vítor Galvão <info@vitorgalvao.com>
2019-01-03 00:31:11 +0000

    Update podbook.rb

--- podbook.rb
+++ podbook.rb
@@ -1,4 +1,6 @@
 class Podbook < Formula
+  include Language::Python::Virtualenv
+
   resource "eyeD3" do
     url "https://files.pythonhosted.org/packages/67/31/8eabaf7abca9ccf8dc4fbe23fb3ad6b363249a3138d79ab8bed34fc36598/eyeD3-0.8.8.tar.gz"
     sha256 "58d18f4313c906c4f88831138fbaf440fca89dcf5a835caa3f67d4efe7d7f4a4"
