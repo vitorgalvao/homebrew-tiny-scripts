e6445932fe818c77d7ef76d835b33d94a50c8dd0
Vítor Galvão <info@vitorgalvao.com>
2019-01-04 01:17:19 +0000

    Update cask-repair.rb

--- cask-repair.rb
+++ cask-repair.rb
@@ -5,7 +5,7 @@ class CaskRepair < Formula
   desc 'Script to quickly repair outdated/broken Casks from homebrew-cask'
   homepage 'https://github.com/vitorgalvao/tiny-scripts'
   url 'https://github.com/vitorgalvao/tiny-scripts.git'
-  version '0.40.1'
+  version '0.40.2'
 
   def install
     bin.install 'cask-repair'
