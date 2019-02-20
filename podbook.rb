c9481c5f72a020112fcc1382d34d8c800fc7daf6
Vítor Galvão <info@vitorgalvao.com>
2019-02-20 18:38:08 +0000

    Update podbook.rb

--- podbook.rb
+++ podbook.rb
@@ -1,8 +1,11 @@
 class Podbook < Formula
+  depends_on 'exiftool'
+  depends_on 'vitorgalvao/tiny-scripts/human-media-time'
+
   desc 'Script to generate a podcast RSS feed for audiobooks'
   homepage 'https://github.com/vitorgalvao/tiny-scripts'
   url 'https://github.com/vitorgalvao/tiny-scripts.git'
-  version '0.13.7'
+  version '0.14.0'
 
   def install
     bin.install 'podbook'
