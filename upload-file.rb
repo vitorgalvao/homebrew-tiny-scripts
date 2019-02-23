ff6c72b4dffe925fdcb04107c4a20370f06a40b6
Vítor Galvão <info@vitorgalvao.com>
2019-02-23 02:41:12 +0000

    Create upload-file.rb

---
+++ upload-file.rb
@@ -0,0 +1,10 @@
+class UploadFile < Formula
+  desc 'Script to upload paths, with support for several file hosts'
+  homepage 'https://github.com/vitorgalvao/tiny-scripts'
+  url 'https://github.com/vitorgalvao/tiny-scripts.git'
+  version '0.0.1'
+
+  def install
+    bin.install 'upload-file'
+  end
+end
