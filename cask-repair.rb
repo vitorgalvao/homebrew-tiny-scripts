beac9fa9330c1a1f435ad023ffd26d3634a693d9
Birger J. Nordølum <contact@mindtooth.no>
2019-03-12 00:09:49 +0100

    cask-repair: Fix Homebrew warnings (#2)

--- cask-repair.rb
+++ cask-repair.rb
@@ -1,13 +1,13 @@
 class CaskRepair < Formula
-  depends_on 'hub'
-  depends_on 'hr' => :recommended
+  desc "Script to quickly repair outdated/broken Casks from homebrew-cask"
+  homepage "https://github.com/vitorgalvao/tiny-scripts"
+  url "https://github.com/vitorgalvao/tiny-scripts.git"
+  version "0.40.6"
 
-  desc 'Script to quickly repair outdated/broken Casks from homebrew-cask'
-  homepage 'https://github.com/vitorgalvao/tiny-scripts'
-  url 'https://github.com/vitorgalvao/tiny-scripts.git'
-  version '0.40.6'
+  depends_on "hub"
+  depends_on "hr" => :recommended
 
   def install
-    bin.install 'cask-repair'
+    bin.install "cask-repair"
   end
 end
