cba3c62275214cb5f63171cc18784e59232a6c3c
Vítor Galvão <info@vitorgalvao.com>
2019-03-23 15:43:40 +0000

    Update cask-analytics.rb

--- cask-analytics.rb
+++ cask-analytics.rb
@@ -2,7 +2,7 @@ class CaskAnalytics < Formula
   desc 'Script to show analytics information for casks in the main taps'
   homepage 'https://github.com/vitorgalvao/tiny-scripts'
   url 'https://github.com/vitorgalvao/tiny-scripts.git'
-  version '0.0.2'
+  version '0.0.3'
 
   def install
     bin.install 'cask-analytics'
