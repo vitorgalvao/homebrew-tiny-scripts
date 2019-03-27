eb13bd1c91a78f1cedd50bc4b9df4cb0648a06a4
Vítor Galvão <info@vitorgalvao.com>
2019-03-27 16:49:49 +0000

    Update workflows-renamer.rb

--- workflows-renamer.rb
+++ workflows-renamer.rb
@@ -2,7 +2,7 @@ class WorkflowsRenamer < Formula
   desc 'Script to rename unpackaged Alfred Workflows in the preferences directory after their name and bundle ID'
   homepage 'https://github.com/vitorgalvao/tiny-scripts'
   url 'https://github.com/vitorgalvao/tiny-scripts.git'
-  version '0.0.1'
+  version '0.0.2'
 
   def install
     bin.install 'workflows-renamer'
