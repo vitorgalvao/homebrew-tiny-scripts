a8f5b3434115b9c86478cae8e54b66cbf5b22843
Vítor Galvão <info@vitorgalvao.com>
2019-03-27 19:47:24 +0000

    Create alfred-rebuild-sharedresources.rb

---
+++ alfred-rebuild-sharedresources.rb
@@ -0,0 +1,11 @@
+
+class AlfredRebuildSharedresources < Formula
+  desc 'Script to update _shared_resources in all my Workflows'
+  homepage 'https://github.com/vitorgalvao/tiny-alfred-scripts'
+  url 'https://github.com/vitorgalvao/alfred-tiny-scripts.git'
+  version '0.0.1'
+
+  def install
+    bin.install 'alfred-rebuild-sharedresources'
+  end
+end
