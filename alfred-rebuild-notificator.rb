d29fd94bacf059fd53bca6b2926898a90820fce0
Vítor Galvão <info@vitorgalvao.com>
2019-03-27 19:46:31 +0000

    Create alfred-rebuild-notificator.rb

---
+++ alfred-rebuild-notificator.rb
@@ -0,0 +1,11 @@
+
+class AlfredRebuildNotificator < Formula
+  desc 'Script to update Notificator in all my Workflows'
+  homepage 'https://github.com/vitorgalvao/tiny-alfred-scripts'
+  url 'https://github.com/vitorgalvao/alfred-tiny-scripts.git'
+  version '0.0.1'
+
+  def install
+    bin.install 'alfred-rebuild-notificator'
+  end
+end
