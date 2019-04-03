4ba41e02a0d03ce7bdf23f48884b7105f20af3be
Vítor Galvão <info@vitorgalvao.com>
2019-04-04 00:48:55 +0100

    Update climergebutton.rb

--- climergebutton.rb
+++ climergebutton.rb
@@ -2,7 +2,7 @@ class Climergebutton < Formula
   desc 'Script to merge githubpull requests via the merge button API'
   homepage 'https://github.com/vitorgalvao/tiny-scripts'
   url 'https://github.com/vitorgalvao/tiny-scripts.git'
-  version '0.1.0'
+  version '0.1.1'
 
   def install
     bin.install 'climergebutton'
