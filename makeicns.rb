3d6fc505df5f0e91de8cd9d60559cde1a60137cf
Vítor Galvão <info@vitorgalvao.com>
2019-05-25 20:47:30 +0100

    Create makeicns.rb

---
+++ makeicns.rb
@@ -0,0 +1,10 @@
+class Makeicns < Formula
+  desc 'Script to make an icns file from a png'
+  homepage 'https://github.com/vitorgalvao/tiny-scripts'
+  url 'https://github.com/vitorgalvao/tiny-scripts.git'
+  version '0.0.1'
+
+  def install
+    bin.install 'makeicns'
+  end
+end
