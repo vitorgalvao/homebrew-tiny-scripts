22b81a1bbabb70b7f61f028f6a7d550f513e9b02
Vítor Galvão <info@vitorgalvao.com>
2019-05-25 01:47:39 +0100

    Update alfred-workflow-update.rb

--- alfred-workflow-update.rb
+++ alfred-workflow-update.rb
@@ -1,5 +1,5 @@
 class AlfredWorkflowUpdate < Formula
-  desc 'Script to update and package a Workflow from the Workflows directory to the vitorgalvao/alfred-workflows github repo'
+  desc 'Script to package and update a Workflow from the Workflows directory to the vitorgalvao/alfred-workflows github repo'
   homepage 'https://github.com/vitorgalvao/tiny-alfred-scripts'
   url 'https://github.com/vitorgalvao/tiny-alfred-scripts.git'
   version '0.0.1'
