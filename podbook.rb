b5adaff56f8cd2f1cc1117c1e7307c10546f4ed0
Vítor Galvão <info@vitorgalvao.com>
2019-11-15 13:39:32 +0000

    Update podbook.rb

--- podbook.rb
+++ podbook.rb
@@ -2,7 +2,7 @@ class Podbook < Formula
   desc "Generate a podcast RSS feed for audiobooks"
   homepage "https://github.com/vitorgalvao/tiny-scripts"
   url "https://github.com/vitorgalvao/tiny-scripts.git"
-  version "0.15.0"
+  version "0.15.1"
 
   depends_on "exiftool"
   depends_on "vitorgalvao/tiny-scripts/human-media-time"
