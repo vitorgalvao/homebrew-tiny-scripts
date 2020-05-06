793fdc274af0ab3bdada4edcb95b0c75ff30c1ba
Vítor Galvão <info@vitorgalvao.com>
2020-05-06 19:42:57 +0100

    Update seren.rb

--- seren.rb
+++ seren.rb
@@ -2,7 +2,7 @@ class Seren < Formula
   desc "Rename files in a numerical sequence"
   homepage "https://github.com/vitorgalvao/tiny-scripts"
   url "https://github.com/vitorgalvao/tiny-scripts.git"
-  version "0.3.2"
+  version "0.3.3"
 
   def install
     bin.install "seren"
