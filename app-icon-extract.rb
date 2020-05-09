f28e40bb374bbb578531aeca094edad2759c243c
Vítor Galvão <info@vitorgalvao.com>
2020-05-10 00:45:39 +0100

    Create app-icon-extract.rb

---
+++ app-icon-extract.rb
@@ -0,0 +1,10 @@
+class AppIconExtract < Formula
+  desc "Extract app bundle icon as png"
+  homepage "https://github.com/vitorgalvao/tiny-scripts"
+  url "https://github.com/vitorgalvao/tiny-scripts.git"
+  version "0.0.1"
+
+  def install
+    bin.install "app-icon-extract"
+  end
+end
