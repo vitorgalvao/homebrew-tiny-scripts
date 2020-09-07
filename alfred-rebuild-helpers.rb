2f964f78ba5c326799d28367527f573bf9870ab9
Vítor Galvão <info@vitorgalvao.com>
2020-09-07 16:15:18 +0100

    Update and rename alfred-rebuild-sharedresources.rb to alfred-rebuild-helpers.rb

---
+++ alfred-rebuild-helpers.rb
@@ -0,0 +1,10 @@
+class AlfredRebuildHelpers < Formula
+  desc 'In all Workflows with "Vítor Galvão" as the creator, update helpers'
+  homepage "https://github.com/vitorgalvao/tiny-alfred-scripts"
+  url "https://github.com/vitorgalvao/tiny-alfred-scripts.git"
+  version "0.0.1"
+
+  def install
+    bin.install "alfred-rebuild-helpers"
+  end
+end
