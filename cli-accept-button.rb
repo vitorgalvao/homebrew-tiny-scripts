ad254767d1a472b7430ca4ba5234804fc536a7ca
Vítor Galvão <info@vitorgalvao.com>
2020-09-08 12:59:23 +0100

    Update cli-accept-button.rb

--- cli-accept-button.rb
+++ cli-accept-button.rb
@@ -1,5 +1,5 @@
 class CliAcceptButton < Formula
-  desc "Accept github pull requests via the review button API"
+  desc "Accept GitHub pull requests via the review button API"
   homepage "https://github.com/vitorgalvao/tiny-scripts"
   url "https://github.com/vitorgalvao/tiny-scripts.git"
   version "0.0.1"
