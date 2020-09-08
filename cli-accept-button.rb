81d70cb8c81347f38404e5342988b1aa3d0a0ab5
Vítor Galvão <info@vitorgalvao.com>
2020-09-08 12:55:15 +0100

    Create cli-accept-button.rb

---
+++ cli-accept-button.rb
@@ -0,0 +1,10 @@
+class CliAcceptButton < Formula
+  desc "Accept github pull requests via the review button API"
+  homepage "https://github.com/vitorgalvao/tiny-scripts"
+  url "https://github.com/vitorgalvao/tiny-scripts.git"
+  version "0.0.1"
+
+  def install
+    bin.install "cli-accept-button"
+  end
+end
