5c14caf79957e57306b45931d512654210876ed3
Vítor Galvão <info@vitorgalvao.com>
2020-09-09 13:26:40 +0100

    Update and rename cli-accept-button.rb to cli-approve-button.rb

--- cli-approve-button.rb
+++ cli-approve-button.rb
@@ -1,5 +1,5 @@
-class CliAcceptButton < Formula
-  desc "Accept GitHub pull requests via the review button API"
+class CliApproveButton < Formula
+  desc "Approve GitHub pull requests via the review button API"
   homepage "https://github.com/vitorgalvao/tiny-scripts"
   url "https://github.com/vitorgalvao/tiny-scripts.git"
   version "0.0.1"
