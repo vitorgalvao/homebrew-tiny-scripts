f19eabd015d81370fa6f9cb48fd010e03c88777b
Robert Blair <12276457+robertleeblairjr@users.noreply.github.com>
2020-11-28 10:50:32 -0600

    Update human-media-time.rb
    Last style check warning for exceeding 80 character length of descriptions.

--- human-media-time.rb
+++ human-media-time.rb
@@ -1,5 +1,5 @@
 class HumanMediaTime < Formula
-  desc "Calculate the total running time of multiple media files, presenting the output in a human-readable way"
+  desc "Calculate total running time of media files & present human-readable output"
   homepage "https://github.com/vitorgalvao/tiny-scripts"
   url "https://github.com/vitorgalvao/tiny-scripts.git"
   version "0.4.1"
