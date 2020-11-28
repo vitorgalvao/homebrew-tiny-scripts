fc1c11aa3c610e9384e51c053811839fcbd62fe1
Vítor Galvão <info@vitorgalvao.com>
2020-11-28 19:55:30 +0000

    Update alfred-placeholder-workflows.rb

--- alfred-placeholder-workflows.rb
+++ alfred-placeholder-workflows.rb
@@ -1,5 +1,5 @@
 class AlfredPlaceholderWorkflows < Formula
-  desc "Create dummy Alfred Workflows that update to their real counterparts, to host on Packal"
+  desc "Create dummy Alfred Workflows which update to the real ones, to host on Packal"
   homepage "https://github.com/vitorgalvao/tiny-alfred-scripts"
   url "https://github.com/vitorgalvao/tiny-alfred-scripts.git"
   version "0.0.1"
