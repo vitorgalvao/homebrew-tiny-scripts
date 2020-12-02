9adafef42499872eb53205c92c453f8a826a33b3
Vítor Galvão <info@vitorgalvao.com>
2020-12-02 21:46:50 +0000

    Update pkg-extract.rb

--- pkg-extract.rb
+++ pkg-extract.rb
@@ -2,7 +2,7 @@ class PkgExtract < Formula
   desc "Extract pkg files in a logical hierarchy"
   homepage "https://github.com/vitorgalvao/tiny-scripts"
   url "https://github.com/vitorgalvao/tiny-scripts.git"
-  version "0.0.4"
+  version "0.0.5"
 
   def install
     bin.install "pkg-extract"
