71efafbcb44038de523e28549acd4175245c212d
Vítor Galvão <info@vitorgalvao.com>
2021-01-02 01:13:36 +0000

    Update and rename pinboardbackup.rb to pinboard-backup.rb

--- pinboard-backup.rb
+++ pinboard-backup.rb
@@ -1,10 +1,10 @@
-class Pinboardbackup < Formula
-  desc "Backup all bookmarks in your pinboard account"
+class PinboardBackup < Formula
+  desc "Backup all bookmarks from a pinboard account"
   homepage "https://github.com/vitorgalvao/tiny-scripts"
   url "https://github.com/vitorgalvao/tiny-scripts.git"
-  version "0.0.6"
+  version "0.1.0"
 
   def install
-    bin.install "pinboardbackup"
+    bin.install "pinboard-backup"
   end
 end
