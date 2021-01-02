aa81f1a0925908ce962fab396b6bef0ab6642fc0
Vítor Galvão <info@vitorgalvao.com>
2021-01-02 01:21:58 +0000

    Update and rename pinboardurlupdate.rb to pinboard-url-update.rb

--- pinboard-url-update.rb
+++ pinboard-url-update.rb
@@ -1,10 +1,10 @@
-class Pinboardurlupdate < Formula
+class PinboardUrlUpdate < Formula
   desc "Substitute URLs of pinboard bookmarks"
   homepage "https://github.com/vitorgalvao/tiny-scripts"
   url "https://github.com/vitorgalvao/tiny-scripts.git"
-  version "0.0.4"
+  version "0.1.0"
 
   def install
-    bin.install "pinboardurlupdate"
+    bin.install "pinboard-url-update"
   end
 end
