1d67d63f4a18962c82953124b0178b99638da7fb
Vítor Galvão <info@vitorgalvao.com>
2021-03-09 02:34:28 +0000

    Update calm-notifications.rb

--- calm-notifications.rb
+++ calm-notifications.rb
@@ -2,7 +2,7 @@ class CalmNotifications < Formula
   desc "Enable, disable, toggle, and show status of Do Not Disturb on macOS"
   homepage "https://github.com/vitorgalvao/tiny-scripts"
   url "https://github.com/vitorgalvao/tiny-scripts.git"
-  version "0.0.1"
+  version "2021.1"
 
   def install
     bin.install "calm-notifications"
