b7f79bd742d4a0c1b8ea6d6e265fa681370255e4
Vítor Galvão <info@vitorgalvao.com>
2022-01-09 19:31:10 +0000

    Update rss-item-sort.rb

--- rss-item-sort.rb
+++ rss-item-sort.rb
@@ -1,7 +1,7 @@
 class RssItemSort < Formula
   desc "Sort RSS items by number in title and upload the result to temporary hosting"
-  homepage "https://github.com/vitorgalvao/tiny-alfred-scripts"
-  url "https://github.com/vitorgalvao/tiny-alfred-scripts.git"
+  homepage "https://github.com/vitorgalvao/tiny-scripts"
+  url "https://github.com/vitorgalvao/tiny-scripts.git"
   version "0.0.1"
 
   def install
