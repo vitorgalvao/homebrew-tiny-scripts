609910bd8e95b78fa8670b487d5bbd6f482dceb2
Vítor Galvão <info@vitorgalvao.com>
2022-02-01 16:25:20 +0000

    Update lossless-compress.rb

--- lossless-compress.rb
+++ lossless-compress.rb
@@ -2,12 +2,11 @@ class LosslessCompress < Formula
   desc "Losslessly compress files"
   homepage "https://github.com/vitorgalvao/tiny-scripts"
   url "https://github.com/vitorgalvao/tiny-scripts.git"
-  version "0.0.3"
+  version "0.0.4"
 
   depends_on "gifsicle"
   depends_on "jpeg"
   depends_on "optipng"
-  depends_on "svgcleaner"
 
   def install
     bin.install "lossless-compress"
