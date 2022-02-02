669231965c6d890cfffbcda92d5df294cec434aa
Vítor Galvão <info@vitorgalvao.com>
2022-02-02 19:27:27 +0000

    tape.rb: Use main branch

--- tape.rb
+++ tape.rb
@@ -1,7 +1,7 @@
 class Tape < Formula
   desc "Backup and restore software settings on macOS"
   homepage "https://github.com/vitorgalvao/tape"
-  url "https://github.com/vitorgalvao/tape.git"
+  url "https://github.com/vitorgalvao/tape.git", branch: "main"
   version "2022.1"
 
   def install
