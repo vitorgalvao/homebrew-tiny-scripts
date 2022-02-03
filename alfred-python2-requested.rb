3a67b38aeb1127b3bd43fb2279cc4c0c45e41e5a
Vítor Galvão <info@vitorgalvao.com>
2022-02-03 18:30:01 +0000

    Create alfred-python2-requested.rb

---
+++ alfred-python2-requested.rb
@@ -0,0 +1,10 @@
+class AlfredPython2Requested < Formula
+  desc "Add Workflow to the alfredapp/updated-third-party-python2-workflows repo"
+  homepage "https://github.com/vitorgalvao/tiny-alfred-scripts"
+  url "https://github.com/vitorgalvao/tiny-alfred-scripts.git"
+  version "0.0.1"
+
+  def install
+    bin.install "alfred-python2-requested"
+  end
+end
