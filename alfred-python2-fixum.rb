52943be9120a22b1199e586046c6054bce1bce5c
Vítor Galvão <info@vitorgalvao.com>
2022-02-09 18:20:55 +0000

    alfred-python2-fixum.rb: Added

---
+++ alfred-python2-fixum.rb
@@ -0,0 +1,10 @@
+class AlfredPython2Fixum < Formula
+  desc "Short automations to help fix Alfred Python 2 Workflows"
+  homepage "https://github.com/vitorgalvao/tiny-alfred-scripts"
+  url "https://github.com/vitorgalvao/tiny-alfred-scripts.git"
+  version "0.0.1"
+
+  def install
+    bin.install "alfred-python2-fixum"
+  end
+end
