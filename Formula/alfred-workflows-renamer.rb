5b938ce312d500ca12dda1d416e6b8f7a20decfe
Vítor Galvão <info@vitorgalvao.com>
2022-03-29 02:07:33 +0100

    Update alfred-workflows-renamer.rb

--- Formula/alfred-workflows-renamer.rb
+++ Formula/alfred-workflows-renamer.rb
@@ -2,7 +2,7 @@ class AlfredWorkflowsRenamer < Formula
   desc "Rename Alfred Workflows in preferences directory to their name and bundle ID"
   homepage "https://github.com/vitorgalvao/tiny-alfred-scripts"
   url "https://github.com/vitorgalvao/tiny-alfred-scripts.git"
-  version "2022.3"
+  version "2022.4"
 
   def install
     bin.install "alfred-workflows-renamer"
