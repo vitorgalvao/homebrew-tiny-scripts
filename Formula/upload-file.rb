733cfab965a9be8979e9b1239238e5863e389ecd
Vítor Galvão <info@vitorgalvao.com>
2023-04-10 13:56:16 +0100

    Update upload-file.rb

--- Formula/upload-file.rb
+++ Formula/upload-file.rb
@@ -2,7 +2,7 @@ class UploadFile < Formula
   desc "Upload paths, with support for several file hosts"
   homepage "https://github.com/vitorgalvao/tiny-scripts"
   url "https://github.com/vitorgalvao/tiny-scripts.git"
-  version "2022.2"
+  version "2023.1"
 
   def install
     bin.install "upload-file"
