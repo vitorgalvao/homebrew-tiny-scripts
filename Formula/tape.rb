eebe835d30275b0c894ce395eb2513e966eb656b
Vítor Galvão <info@vitorgalvao.com>
2023-10-10 11:22:36 +0100

    Update tape.rb

--- Formula/tape.rb
+++ Formula/tape.rb
@@ -2,7 +2,7 @@ class Tape < Formula
   desc "Backup and restore software settings on macOS"
   homepage "https://github.com/vitorgalvao/tape"
   url "https://github.com/vitorgalvao/tape.git", branch: "main"
-  version "2022.4"
+  version "2023.1"
 
   depends_on "ruby"
 
