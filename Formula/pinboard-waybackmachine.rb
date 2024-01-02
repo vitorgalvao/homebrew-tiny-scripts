b3f636366a7230b26e10750fc93413688013dfff
Vítor Galvão <info@vitorgalvao.com>
2024-01-02 17:11:53 +0000

    Update pinboard-waybackmachine.rb

--- Formula/pinboard-waybackmachine.rb
+++ Formula/pinboard-waybackmachine.rb
@@ -2,7 +2,7 @@ class PinboardWaybackmachine < Formula
   desc "Add to the Wayback Machine links saved to a pinboard account"
   homepage "https://github.com/vitorgalvao/tiny-pinboard-scripts"
   url "https://github.com/vitorgalvao/tiny-pinboard-scripts.git"
-  version "2023.1"
+  version "2024.1"
 
   def install
     bin.install "pinboard-waybackmachine"
