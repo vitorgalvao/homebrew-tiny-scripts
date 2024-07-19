88ed0ae239cf65752ba410c3b2d453597749f637
Vítor Galvão <info@vitorgalvao.com>
2024-07-19 17:24:43 +0100

    Update and rename launch.rb to laus.rb

--- Formula/laus.rb
+++ Formula/laus.rb
@@ -1,10 +1,10 @@
-class Launch < Formula
+class Laus < Formula
   desc "Shortcuts for launchctl actions"
   homepage "https://github.com/vitorgalvao/tiny-scripts"
   url "https://github.com/vitorgalvao/tiny-scripts.git"
   version "2024.1"
 
   def install
-    bin.install "launch"
+    bin.install "laus"
   end
 end
