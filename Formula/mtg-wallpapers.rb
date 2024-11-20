fa00564fd047d5643d8bed7f052679639cc4b628
Vítor Galvão <info@vitorgalvao.com>
2024-11-20 23:14:15 +0000

    Update mtg-wallpapers.rb

--- Formula/mtg-wallpapers.rb
+++ Formula/mtg-wallpapers.rb
@@ -2,7 +2,7 @@ class MtgWallpapers < Formula
   desc "Download Magic: The Gathering wallpapers"
   homepage "https://github.com/vitorgalvao/tiny-scripts"
   url "https://github.com/vitorgalvao/tiny-scripts.git"
-  version "2022.2"
+  version "2024.1"
 
   def install
     bin.install "mtg-wallpapers"
