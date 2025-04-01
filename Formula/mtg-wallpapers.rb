2f812998f5e1e59b17e1a0e712da5e68a84f87e2
Vítor Galvão <info@vitorgalvao.com>
2025-04-01 23:33:39 +0100

    mtg-wallpapers.rb: Updated

--- Formula/mtg-wallpapers.rb
+++ Formula/mtg-wallpapers.rb
@@ -1,11 +1,11 @@
 class MtgWallpapers < Formula
   desc "Download Magic: The Gathering wallpapers"
-  homepage "https://github.com/vitorgalvao/tiny-scripts"
-  url "https://github.com/vitorgalvao/tiny-scripts.git"
+  homepage "https://github.com/vitorgalvao/mtg-wallpapers"
+  url "https://github.com/vitorgalvao/mtg-wallpapers.git", branch: "main"
   version "2024.1"
 
   def install
     bin.install "mtg-wallpapers"
-    man1.install "manpages/mtg-wallpapers.1"
+    man1.install "manpage/mtg-wallpapers.1"
   end
 end
