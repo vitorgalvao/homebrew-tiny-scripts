b8eb6a534020c182f9454033c009885d0d4facc2
Vítor Galvão <info@vitorgalvao.com>
2025-04-02 00:20:48 +0100

    photo-date-rename.rb: Update

--- Formula/photo-date-rename.rb
+++ Formula/photo-date-rename.rb
@@ -1,7 +1,7 @@
 class PhotoDateRename < Formula
-  desc "Rename image files to the date and time they were taken"
-  homepage "https://github.com/vitorgalvao/tiny-scripts"
-  url "https://github.com/vitorgalvao/tiny-scripts.git"
+  desc "Rename digital photographs to the date and time they were taken"
+  homepage "https://github.com/vitorgalvao/photo-date-rename"
+  url "https://github.com/vitorgalvao/photo-date-rename.git", branch: "main"
   version "2023.1"
 
   def install
