085185c6870e83e809e67b8795f8f2c5b3803137
Vítor Galvão <info@vitorgalvao.com>
2025-04-02 01:16:06 +0100

    Rename makeicns

---
+++ Formula/genicns.rb
@@ -0,0 +1,10 @@
+class Genicns < Formula
+  desc "Generate an icns icon from a png"
+  homepage "https://github.com/vitorgalvao/genicns"
+  url "https://github.com/vitorgalvao/genicns.git"
+  version "2025.1"
+
+  def install
+    bin.install "genicns"
+  end
+end
