098b73058496d592cb9d2cec3e214788bddba09b
Vítor Galvão <info@vitorgalvao.com>
2025-04-10 13:14:46 +0100

    ql.rb: Initial version

---
+++ Formula/ql.rb
@@ -0,0 +1,10 @@
+class Ql < Formula
+  desc "Preview paths with Quick Look"
+  homepage "https://github.com/vitorgalvao/ql"
+  url "https://github.com/vitorgalvao/ql/releases/latest/download/ql"
+  version "2025.1"
+
+  def install
+    bin.install "ql"
+  end
+end
