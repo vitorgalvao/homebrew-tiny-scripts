ab70693cfb7dc5c9ea713cb470ed5572634a80e5
Vítor Galvão <info@vitorgalvao.com>
2025-04-10 13:18:46 +0100

    ql.rb: version before sha256

--- Formula/ql.rb
+++ Formula/ql.rb
@@ -2,8 +2,8 @@ class Ql < Formula
   desc "Preview paths with Quick Look"
   homepage "https://github.com/vitorgalvao/ql"
   url "https://github.com/vitorgalvao/ql/releases/latest/download/ql"
-  sha256 "f31a7aa00802b7db32d9cda9fd62295ba67af4901b22755ea48742657fbda30f"
   version "2025.1"
+  sha256 "f31a7aa00802b7db32d9cda9fd62295ba67af4901b22755ea48742657fbda30f"
 
   def install
     bin.install "ql"
