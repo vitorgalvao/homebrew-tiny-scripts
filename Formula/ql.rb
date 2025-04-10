071f371ec9cbccc220231971e516252f63a7818c
Vítor Galvão <info@vitorgalvao.com>
2025-04-10 13:17:49 +0100

    ql.rb: Add checksum

--- Formula/ql.rb
+++ Formula/ql.rb
@@ -2,6 +2,7 @@ class Ql < Formula
   desc "Preview paths with Quick Look"
   homepage "https://github.com/vitorgalvao/ql"
   url "https://github.com/vitorgalvao/ql/releases/latest/download/ql"
+  sha256 "f31a7aa00802b7db32d9cda9fd62295ba67af4901b22755ea48742657fbda30f"
   version "2025.1"
 
   def install
