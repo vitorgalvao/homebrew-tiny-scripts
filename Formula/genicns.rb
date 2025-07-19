31e96280431deb82944a511c092c064a14d4a2a5
Vítor Galvão <info@vitorgalvao.com>
2025-07-19 21:10:22 +0100

    Update genicns.rb

--- Formula/genicns.rb
+++ Formula/genicns.rb
@@ -1,7 +1,7 @@
 class Genicns < Formula
   desc "Generate an icns icon from a png"
   homepage "https://github.com/vitorgalvao/genicns"
-  url "https://github.com/vitorgalvao/genicns.git"
+  url "https://github.com/vitorgalvao/genicns.git", branch: "main"
   version "2025.1"
 
   def install
