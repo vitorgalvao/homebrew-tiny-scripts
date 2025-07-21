66d09c74d1a285b62d17aaee2772282f43934940
Vítor Galvão <info@vitorgalvao.com>
2025-07-21 15:13:18 +0100

    Update upload-file.rb

--- Formula/upload-file.rb
+++ Formula/upload-file.rb
@@ -2,7 +2,7 @@ class UploadFile < Formula
   desc "Upload paths, with support for several file hosts"
   homepage "https://github.com/vitorgalvao/upload-file"
   url "https://github.com/vitorgalvao/upload-file.git", branch: "main"
-  version "25.1"
+  version "2025.2"
 
   def install
     bin.install "upload-file"
