78b7f906546f6f06f6021443c99c57cef120d830
Vítor Galvão <info@vitorgalvao.com>
2025-09-19 17:23:46 +0100

    Update audiobookify.rb

--- Formula/audiobookify.rb
+++ Formula/audiobookify.rb
@@ -2,7 +2,7 @@ class Audiobookify < Formula
   desc "Concatenate folder of audio files into a single M4B audiobook file with chapters"
   homepage "https://github.com/vitorgalvao/audiobookify"
   url "https://github.com/vitorgalvao/audiobookify.git", branch: "main"
-  version "2025.1"
+  version "2025.2"
 
   depends_on "mp4v2"
   depends_on "ffmpeg"
