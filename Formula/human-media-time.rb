28c150b69e924c0f7108be50c0745feae975c6a1
Vítor Galvão <info@vitorgalvao.com>
2025-09-19 17:25:56 +0100

    Update human-media-time.rb

--- Formula/human-media-time.rb
+++ Formula/human-media-time.rb
@@ -2,7 +2,7 @@ class HumanMediaTime < Formula
   desc "Calculate and present total running time of media files in a human-readable way"
   homepage "https://github.com/vitorgalvao/human-media-time"
   url "https://github.com/vitorgalvao/human-media-time.git", branch: "main"
-  version "2025.1"
+  version "2025.2"
 
   depends_on "ffmpeg"
 
