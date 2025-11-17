e4d983c1649da49e0e49b09f08a84d34d3e30652
Vítor Galvão <info@vitorgalvao.com>
2025-11-17 17:46:46 +0000

    Update laus.rb

--- Formula/laus.rb
+++ Formula/laus.rb
@@ -2,7 +2,7 @@ class Laus < Formula
   desc "Shortcuts for launchctl actions"
   homepage "https://github.com/vitorgalvao/laus"
   url "https://github.com/vitorgalvao/laus.git", branch: "main"
-  version "2025.1"
+  version "2025.2"
 
   def install
     bin.install "laus"
