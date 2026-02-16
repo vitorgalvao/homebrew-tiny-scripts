283aed6c76a05085573b635fb6f15c074af324f4
Vítor Galvão <info@vitorgalvao.com>
2026-02-16 18:06:57 +0100

    Update gh-action-builder.rb

--- Formula/gh-action-builder.rb
+++ Formula/gh-action-builder.rb
@@ -2,7 +2,7 @@ class GhActionBuilder < Formula
   desc "Build a sane initial GitHub Actions YAML by answering questions"
   homepage "https://github.com/vitorgalvao/gh-action-builder"
   url "https://github.com/vitorgalvao/gh-action-builder.git", branch: "main"
-  version "26.1"
+  version "26.2"
 
   def install
     bin.install "gh-action-builder"
